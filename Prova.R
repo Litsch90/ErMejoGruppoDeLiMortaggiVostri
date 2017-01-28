e2a4c1f2f5ef42c68efe898f43eaeaf6075dd248
litsch90 <luca.licinio@gmail.com>
2017-01-28 14:18:32 +0100

    Aggiornato

--- Prova.R
+++ Prova.R
@@ -1,2 +1,8 @@
 library('GEOquery')
 library('GEOmetadb')
+if(!file.exists('GEOmetadb.sqlite')) getSQLiteFile()
+con <- dbConnect(SQLite(),'GEOmetadb.sqlite')
+geo_tables <- dbListTables(con)
+gsm <- dbGetQuery(con,"SELECT DISTINCT gsm FROM gsm WHERE title LIKE '%human%' AND title LIKE '%breast%' AND title LIKE '%cancer%';")
+numGSM <-nrow(gsm)
+gse <- dbGetQuery(con,"SELECT DISTINCT gse FROM gse WHERE title LIKE '%human%' AND title LIKE '%breast%' AND title LIKE '%cancer%';")
