698c552b5a0fe48daaa746a6cc710d8e29597f40
litsch90 <luca.licinio@gmail.com>
2017-01-31 11:53:20 +0100

    cco lo script con gli input della radiobox

--- Prova.R
+++ Prova.R
@@ -1,8 +1,35 @@
 #Seleziona il file di input dalla lista di file disponibili e lo ritorna
 select <- function(){
   input <- tclvalue(tkgetOpenFile(title="Selezionare il file di input", filetypes = "{ {Text Files} {.txt} }"))
-  stopifnot(input != "")
+  if(input == ""){
+    
+  }
   return(input)
+}
+
+#Questa funzione è da finire
+cerca <- function(specie, key){
+  library('GEOquery')
+  library('GEOmetadb')
+  if(!file.exists('GEOmetadb.sqlite')){
+    getSQLiteFile()
+  }
+  con <- dbConnect(SQLite(),'GEOmetadb.sqlite')
+  geo_tables <- dbListTables(con)
+  query <-paste("
+    SELECT count(*) ",
+    "FROM",
+             "  gsm JOIN gse_gsm ON gsm.gsm=gse_gsm.gsm",
+             "  JOIN gse ON gse_gsm.gse=gse.gse",
+             "  JOIN gse_gpl ON gse_gpl.gse=gse.gse",
+             "  JOIN gpl ON gse_gpl.gpl=gpl.gpl",
+             "WHERE", 
+            " gpl.organism LIKE '%", specie, "%' AND",
+            " gse.title LIKE '%", key, "%' OR",
+            " gse.summary LIKE '%", key, "%';")
+  gsm <- dbGetQuery(con, query)
+  return(gsm)
+  gse <- dbGetQuery(con,"SELECT DISTINCT gse FROM gse WHERE title LIKE '%human%' AND title LIKE '%breast%' AND title LIKE '%cancer%';")
 }
 
 #Crea una fiestra con tre radiobutton per selezionare una specie tra uomo, topo e ratto, poi passa il valore selezionato alla funzione cerca (default "human")
@@ -19,14 +46,11 @@ radio <- function(){
   tkgrid(tklabel(inputBox,text="Human "),rb1)
   tkgrid(tklabel(inputBox,text="Mouse "),rb2)
   tkgrid(tklabel(inputBox,text="Rat "),rb3)
-  #prova
   
   #Una volta premuto il tasto ok dalla finestra di dialogo, invia il valore selezionato ad un'altra funzione
   onOK <- function(){
-    rbVal <- as.character(tclvalue(rbValue))
+    rbVal <<- as.character(tclvalue(rbValue))
     tkdestroy(inputBox)
-    cat(rbVal)
-    #cerca(rbVal)
   }
   
   OK.but <- tkbutton(inputBox,text="OK",command=onOK)
@@ -37,22 +61,18 @@ radio <- function(){
 
 #Qua iniziano i comandi che vengono eseguiti appena lanciato lo script
 library('tcltk')
+rbVal <- ""
+result <- ""
+output1 <- file.create(paste("Prova_", format(Sys.time(), format="%Y-%m-%d%H%M"), ".txt"))
 input <- select()
+inputArray <- read.table(input)
 radio()
-
-#Questa funzione è da finire
-cerca <- function(specie){
-  library('GEOquery')
-  library('GEOmetadb')
-  if(!file.exists('GEOmetadb.sqlite')){
-    getSQLiteFile()
-  }
-  con <- dbConnect(SQLite(),'GEOmetadb.sqlite')
-  geo_tables <- dbListTables(con)
-  gsm <- dbGetQuery(con,"SELECT DISTINCT gsm FROM gsm WHERE title LIKE '%human%' AND title LIKE '%breast%' AND title LIKE '%cancer%';")
-  numGSM <-nrow(gsm)
-  gse <- dbGetQuery(con,"SELECT DISTINCT gse FROM gse WHERE title LIKE '%human%' AND title LIKE '%breast%' AND title LIKE '%cancer%';")
+for (counter in inputArray){
+	result <- c(result, cerca(rbVal, counter))
 }
+
+
+
 
 
 
