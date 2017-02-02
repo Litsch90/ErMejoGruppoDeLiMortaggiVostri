c1e4a10caf14ebb6d0b2393e1d4e352d81c1b39d
litsch90 <luca.licinio@gmail.com>
2017-02-02 23:31:20 +0100

    Ho finito le query, mancano solo un paio di cose domani e siamo apposto

--- Prova.R
+++ Prova.R
@@ -1,121 +1,138 @@
-Prova <- function(outName){
-  
-  #Seleziona il file di input dalla lista di file disponibili e lo ritorna
-  select <- function(){
-    write("created 'select' variable (function).",file = logFile,sep="",append = TRUE)
-    input <- tclvalue(tkgetOpenFile(title="Selezionare il file di input", filetypes = "{ {Text Files} {.txt} }"))
-    write("created 'input' variable internal to the 'select' function.",file = logFile,sep="",append = TRUE)
-    if(input == ""){
-      
-    }
-    return(input)
+#Seleziona il file di input dalla lista di file disponibili e lo ritorna
+select <- function(){
+  writeLog("created 'select' variable (function).")
+  input <- tclvalue(tkgetOpenFile(title="Selezionare il file di input", filetypes = "{ {Text Files} {.txt} }"))
+  writeLog("created 'input' variable internal to the 'select' function.")
+  if(input == ""){
+    writeLog("ERROR: no input file selected")
+    stop()
   }
+  return(input)
+}
+
+#Crea una fiestra con tre radiobutton per selezionare una specie tra uomo, topo e ratto, poi passa il valore selezionato alla funzione cerca (default "human")
+radio <- function(){
+  writeLog("created 'radio' variable(function).")
+  inputBox <- tktoplevel()
+  writeLog("created 'inputBox' variable internal to the 'radio' function.")
+  rb1 <- tkradiobutton(inputBox)
+  writeLog("created 'rb1' variable internal to the 'radio' function.")
+  rb2 <- tkradiobutton(inputBox)
+  writeLog("created 'rb2' variable internal to the 'radio' function.")
+  rb3 <- tkradiobutton(inputBox)
+  writeLog("created 'rb3' variable internal to the 'radio' function.")
+  rbValue <- ""
+  writeLog("created 'rbValue' variable internal to the 'radio' function.")
+  tkconfigure(rb1,variable=rbValue,value="'Homo sapiens'")
+  tkconfigure(rb2,variable=rbValue,value="'Mus musculus'")
+  tkconfigure(rb3,variable=rbValue,value="'Rattus norvegicus'")
+  tkgrid(tklabel(inputBox,text="Which spece you want to analize?"))
+  tkgrid(tklabel(inputBox,text="Human "),rb1)
+  tkgrid(tklabel(inputBox,text="Mouse "),rb2)
+  tkgrid(tklabel(inputBox,text="Rat "),rb3)
   
-  #Crea una fiestra con tre radiobutton per selezionare una specie tra uomo, topo e ratto, poi passa il valore selezionato alla funzione cerca (default "human")
-  radio <- function(){
-    write("created 'radio' variable(function).",file = logFile,sep="",append = TRUE)
-    inputBox <- tktoplevel()
-    write("created 'inputBox' variable internal to the 'radio' function.",file = logFile,sep="",append = TRUE)
-    rb1 <- tkradiobutton(inputBox)
-    write("created 'rb1' variable internal to the 'radio' function.",file = logFile,sep="",append = TRUE)
-    rb2 <- tkradiobutton(inputBox)
-    write("created 'rb2' variable internal to the 'radio' function.",file = logFile,sep="",append = TRUE)
-    rb3 <- tkradiobutton(inputBox)
-    write("created 'rb3' variable internal to the 'radio' function.",file = logFile,sep="",append = TRUE)
-    rbValue <- tclVar("Homo sapiens")
-    write("created 'rbValue' variable internal to the 'radio' function.",file = logFile,sep="",append = TRUE)
-    tkconfigure(rb1,variable=rbValue,value="Homo sapiens")
-    tkconfigure(rb2,variable=rbValue,value="Mus musculus")
-    tkconfigure(rb3,variable=rbValue,value="Rattus norvegicus")
-    tkgrid(tklabel(inputBox,text="Which spece you want to analize?"))
-    tkgrid(tklabel(inputBox,text="Human "),rb1)
-    tkgrid(tklabel(inputBox,text="Mouse "),rb2)
-    tkgrid(tklabel(inputBox,text="Rat "),rb3)
-    
-    #Una volta premuto il tasto ok dalla finestra di dialogo, invia il valore selezionato ad un'altra funzione
-    onOK <- function(){
-      write("created 'onOK' variable (function) internal to the 'radio' function.",file = logFile,sep="",append = TRUE)
-      rbVal <<- as.character(tclvalue(rbValue))
-      tkdestroy(inputBox)
-    }
-    
-    OK.but <- tkbutton(inputBox,text="OK",command=onOK)
-    write("created 'OK.but' variable internal to the 'radio' function.",file = logFile,sep="",append = TRUE)
-    tkgrid(OK.but)
-    tkfocus(inputBox)
-  }
-  
-  #Questa funzione è da finire
-  cerca <- function(specie, key){
-    write("created 'cerca' variable (function).",file = logFile,sep="",append = TRUE)
-    library('GEOquery')
-    library('GEOmetadb')
-    if(!file.exists('GEOmetadb.sqlite')){
-      getSQLiteFile()
-    }
-    con <- dbConnect(SQLite(),'GEOmetadb.sqlite')
-    write("created 'con' variable internal to the 'cerca' function.",file = logFile,sep="",append = TRUE)
-    geo_tables <- dbListTables(con)
-    write("created 'geo_tables' variable internal to the 'cerca' function.",file = logFile,sep="",append = TRUE)
-    query <-paste("
-                  SELECT count(*) ",
-                  "FROM",
-                  "  gsm JOIN gse_gsm ON gsm.gsm=gse_gsm.gsm",
-                  "  JOIN gse ON gse_gsm.gse=gse.gse",
-                  "  JOIN gse_gpl ON gse_gpl.gse=gse.gse",
-                  "  JOIN gpl ON gse_gpl.gpl=gpl.gpl",
-                  "WHERE", 
-                  " gpl.organism LIKE '", specie, "' AND",
-                  " gse.title LIKE '%", key, "%' OR",
-                  " gse.summary LIKE '%", key, "%';")
-    write("created 'query' variable internal to the 'cerca' function.",file = logFile,sep="",append = TRUE)
-    if(key == inputArray[1]){
-      gsm <<- as.character(dbGetQuery(con, query)) 
+  #Una volta premuto il tasto ok dalla finestra di dialogo, invia il valore selezionato ad un'altra funzione
+  onOK <- function(){
+    writeLog("created 'onOK' variable (function) internal to the 'radio' function.")
+    rbVal <<- tclvalue(rbValue)
+    tkdestroy(inputBox)
+    if(rbVal == ""){
+        writeLog("ERROR: no spece selected")
+        stop()
     }
     else{
-      gsm <<- as.character(c(gsm, dbGetQuery(con, query))) 
-    }
-    #gse <- dbGetQuery(con,"SELECT DISTINCT gse FROM gse WHERE title LIKE '%human%' AND title LIKE '%breast%' AND title LIKE '%cancer%';")
+        writeLog(paste("Specie:", rbVal, sep = " "))
+        for (counter in inputArray){
+            cerca(rbVal, counter)
+        }
+        writeLog("removed 'counter' variable.")
+        writeOutput(paste(outName,".csv",sep=""))
+        }
   }
   
-  #Questa funzione scrive i risultati dello script in un file di output in un data.frame, per poi scriverli in un file di tipo csv
-  writeOutput <- function(outputFile) {
-    write("created 'writeOutput' variable (function).",file = logFile,sep="",append = TRUE)
-    writable <- data.frame(inputArray, gsm)
-    write("created 'writable' variable internal to the 'writeOutput' function.",file = logFile,sep="",append = TRUE)
-    colnames(writable) <- c("Keywords", "Number of gsm ID")
-    write.csv(writable, outputFile)
-  }
-  
-  #Qua iniziano i comandi che vengono eseguiti appena richiamata la funzione
-  #Controllo se la cartella dei log esiste ed eventualmente la creo
-  if (!file.exists("logs")){
-    dir.create(file.path("logs"))
-  }
-  #Creazione del file di log
-  logName <- paste("Prova", format(Sys.time(), format="%Y-%m-%d%H%M"), ".txt",sep = "_")
-  logFile <- file.path("logs",logName)
-  #Selezione file di input
-  library('tcltk')
-  rbVal <- ""
-  write("Created 'rbVal' variable.",file = logFile,sep="",append = TRUE)
-  input <- select()
-  write("created 'input' variable.",file = logFile,sep="",append = TRUE)
-  write(paste("Input file:",input,sep = " "),file = logFile,sep="")
-  write(paste("Output file:",paste(outName,".csv",sep=""),sep = " "),file = logFile,sep="",append = TRUE)
-  radio()
-  write(paste("Specie:",rbVal,sep = " "),file = log,sep="",append = TRUE)
-  write("created 'logName' variable.",file = logFile,sep="",append = TRUE)
-  write("created 'logFile' variable.",file = logFile,sep="",append = TRUE)
-  gsm <- ""
-  write("created 'gsm' variable.",file = logFile,sep="",append = TRUE)
-  inputArray <- as.character(scan(file=input, what=character(), sep = "\n"))
-  write("created 'inputArray' variable.",file = logFile,sep="",append = TRUE)
-  write("created 'counter' variable internal to the for loop.",file = logFile,sep="",append = TRUE)
-  for (counter in inputArray){
-    cerca(rbVal, counter)
-  }
-  write("removed 'counter' variable.",file = logFile,sep="",append = TRUE)
-  writeOutput(paste(outName,".csv",sep=""))
-  
+  OK.but <- tkbutton(inputBox,text="OK",command=onOK)
+  writeLog("created 'OK.but' variable internal to the 'radio' function.")
+  tkgrid(OK.but)
+  tkfocus(inputBox)
 }
+
+#Questa funzione è da finire
+cerca <- function(specie, key){
+  writeLog("created 'cerca' variable (function).")
+  library('GEOquery')
+  library('GEOmetadb')
+  if(!file.exists('GEOmetadb.sqlite')){
+    getSQLiteFile()
+  }
+  con <- dbConnect(SQLite(),'GEOmetadb.sqlite')
+  writeLog(paste("created 'con' variable internal to the 'cerca' function. Specie:", specie, " chiave: ", key))
+  geo_tables <- dbListTables(con)
+  writeLog("created 'geo_tables' variable internal to the 'cerca' function.")
+  query1 <-paste("SELECT count(gsm.gsm) ",
+                "FROM",
+                "  gsm JOIN gse_gsm ON gsm.gsm=gse_gsm.gsm",
+                "  JOIN gse ON gse_gsm.gse=gse.gse",
+                "  JOIN gse_gpl ON gse_gpl.gse=gse.gse",
+                "  JOIN gpl ON gse_gpl.gpl=gpl.gpl",
+                "WHERE", 
+                " gpl.organism=", specie, " AND (gse.title LIKE '%", key, "%' OR gse.summary LIKE '%", key, "%');")
+  writeLog("created 'query1' variable internal to the 'cerca' function.")
+  query2 <- paste("SELECT gse.gse ",
+                " FROM",
+                "  gsm JOIN gse_gsm ON gsm.gsm=gse_gsm.gsm",
+                "  JOIN gse ON gse_gsm.gse=gse.gse",
+                "  JOIN gse_gpl ON gse_gpl.gse=gse.gse",
+                "  JOIN gpl ON gse_gpl.gpl=gpl.gpl",
+                "WHERE", 
+                " gpl.organism=", specie, " AND (gse.title LIKE '%", key, "%' OR gse.summary LIKE '%", key, "%')",
+                " GROUP BY gse.gse;")
+  if(key == inputArray[1]){
+    gsm <<- as.character(dbGetQuery(con, query1)) 
+    gse <<- dbGetQuery(con, query2)
+  }
+  else{
+    gsm <<- as.character(c(gsm, dbGetQuery(con, query1)))
+    gse <<- as.vector(c(gse, dbGetQuery(con, query2)))
+  }
+  #gse <- dbGetQuery(con,"SELECT DISTINCT gse FROM gse WHERE title LIKE '%human%' AND title LIKE '%breast%' AND title LIKE '%cancer%';")
+}
+
+#Questa funzione scrive i risultati dello script in un file di output in un data.frame, per poi scriverli in un file di tipo csv
+writeOutput <- function(outputFile) {
+  writeLog("created 'writeOutput' variable (function).")
+  writable <- data.frame(inputArray, gsm, gse)
+  writeLog("created 'writable' variable internal to the 'writeOutput' function.")
+  colnames(writable) <- c("Keywords", "Number of gsm ID", "gse ID")
+  write.csv(writable, outputFile)
+}
+
+writeLog <- function(message){
+  write(message,file = logFile ,sep="",append = TRUE)
+}
+
+#Qua iniziano i comandi che vengono eseguiti appena richiamata la funzione
+#Controllo se la cartella dei log esiste ed eventualmente la creo
+outName <- "cazzen"
+if (!file.exists("logs")){
+  dir.create(file.path("logs"))
+}
+#Creazione del file di log
+logName <- paste("Prova_", format(Sys.time(), format="%Y-%m-%d%H%M"), ".txt",sep = "")
+logFile <- file.path("logs",logName)
+#Selezione file di input
+library('tcltk')
+rbVal <- ""
+writeLog("Created 'rbVal' variable.")
+input <- select()
+writeLog("created 'input' variable.")
+writeLog(paste("Input file:",input,sep = " "))
+writeLog(paste("Output file:",paste(outName,".csv",sep=""),sep = " "))
+gsm <- ""
+gse <- ""
+writeLog("created 'gsm' variable.")
+inputArray <- as.character(scan(file=input, what=character(), sep = "\n"))
+writeLog("created 'inputArray' variable.")
+radio()
+writeLog("created 'logName' variable.")
+writeLog("created 'logFile' variable.")
+writeLog("created 'counter' variable internal to the for loop.")
